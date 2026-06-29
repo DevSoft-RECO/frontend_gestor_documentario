102f9bdb146e89b51fb9c9747979cb007d81e91c
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-06-29 09:26:44 -0600

    CORRECION E LA SETIQUETAS DE VIENGIA

--- src/views/Manuales/AdminManualesView.vue
+++ src/views/Manuales/AdminManualesView.vue
@@ -362,15 +362,15 @@ const getVigenciaStatus = (manual: Manual) => {
   today.setHours(0, 0, 0, 0)
   const vigenciaDate = new Date(manual.fecha_vigencia)
   
-  if (vigenciaDate >= today) {
+  if (today >= vigenciaDate) {
     return {
       label: 'Vigente',
       class: 'bg-emerald-50 dark:bg-emerald-950/20 text-emerald-600 dark:text-emerald-400 border border-emerald-100 dark:border-emerald-900/10'
     }
   } else {
     return {
-      label: 'Vencido',
-      class: 'bg-rose-50 dark:bg-rose-950/20 text-rose-600 dark:text-rose-400 border border-rose-100 dark:border-rose-900/10'
+      label: 'No Vigente',
+      class: 'bg-amber-50 dark:bg-amber-950/20 text-amber-600 dark:text-amber-400 border border-amber-100 dark:border-amber-900/10'
     }
   }
 }
